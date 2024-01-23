b67a828441af6a6daf05f0b5ec5d9cad06d6fb7e
Zach Drake <zacharydrake14@gmail.com>
2024-01-23 03:53:25 +0000

    place details within geom

--- PlaceDetailsSearch.jl
+++ PlaceDetailsSearch.jl
@@ -1,43 +1,139 @@
 module PlaceDetailsSearch
+using Base: length_continued
+include("placeTypes.jl")
 
-export get_place_ids
+export placedetails, GoogleAPI, SearchPoint
 
-import HTTP
-import JSON
+import HTTP, JSON, DataFrames, Missings, GeoIO, Meshes, Base.Threads
 
-API_KEY::String = "AIzaSyB7lJ3rfPvqYp8xbHGUEkoSEBx86Qy1lls"
-const BASE_URL::String = "https://places.googleapis.com/v1/"
+@kwdef struct GoogleAPI
+    base::String
+    key::String
+end
 
-places::Vector{Dict{String,Any}} = []
+function _createsearchpoints(area::Meshes.MultiPolygon,
+    api_limit::Integer=4000)::Vector{Meshes.Point2}
+
+    sample_rate::Float64 = 0.0003
+    points::Vector{Any} = zeros(api_limit + 1)
 
 
-function get_place_details()
+    while length(points) > api_limit
+        if length(points) == api_limit + 1
+            println("Building search grid...")
+        else
+            println("Search grid has $(length(points)) points. Building with smaller sample rate.")
+        end
 
-    if length(API_KEY) == 0
-        throw(UndefVarError(:API_KEY))
+        sampler::Meshes.MinDistanceSampling = Meshes.MinDistanceSampling(sample_rate)
+        points = Meshes.sample(area, sampler) |> collect
+        sample_rate += 0.0001
+    end
+
+    println("Search grid has $(length(points)) points.")
+    return points
+end
+
+function placedetails(api::GoogleAPI,
+    area::Meshes.MultiPolygon,
+    api_limit::Integer=nothing)::DataFrames.DataFrame
+
+    search_grid_points::Vector{Meshes.Point2} = []
+    if api_limit == nothing
+        search_grid_points = _createsearchpoints(area)
+    else
+        search_grid_points = _createsearchpoints(area, api_limit)
+    end
+
+
+    place_ids::Vector{Dict{String,Any}} = []
+    println("Running nearby search for each grid point.")
+    for search_point in search_grid_points
+        for i in _nearbysearch(api=api, search_point=search_point)
+            push!(place_ids, i)
+        end
+    end
+
+    flat_places::Vector{Any} = []
+    println("Extracting data.")
+    for place in place_ids
+        push!(flat_places, _flattenplace(place))
+    end
+
+    return DataFrames.DataFrame(flat_places)
+
+end
+
+function _nearbysearch(; api::GoogleAPI, search_point::Meshes.Point)::Vector{Any}
+    coords::Meshes.Vec{2,Float64} = Meshes.coordinates(search_point)
+
+    if length(api.key) == 0
+        throw(UndefVarError(:apikey))
     end
 
     api_headers::Vector{Pair{String,String}} = [
         "Content-Type" => "application/json",
-        "X-Goog-Api-Key" => API_KEY,
+        "X-Goog-Api-Key" => api.key,
         "X-Goog-FieldMask" => "*",
     ]
 
-    home_latlon::Dict{String,Float64} =
-        Dict("latitude" => 39.27538320472095, "longitude" => -76.61034813442848)
-
-    search_circle::Dict{String,Any} = Dict("center" => home_latlon, "radius" => 50)
+    search_circle::Dict{String,Any} = Dict(
+        "center" => Dict{String,Float64}(
+            "latitude" => coords[2],
+            "longitude" => coords[1],
+        ),
+        "radius" => 500)
 
     body::Dict{String,Any} =
-        Dict("locationRestriction" => Dict{String,Dict}("circle" => search_circle))
+        Dict("rankPreference" => "DISTANCE",
+            "locationRestriction" => Dict{String,Dict}(
+                "circle" => search_circle),
+        )
 
     response::HTTP.Messages.Response =
-        HTTP.request(:POST, BASE_URL * "places:searchNearby", api_headers, JSON.json(body))
+        HTTP.request(:POST, api.base * "places:searchNearby", api_headers, JSON.json(body))
 
-    data = JSON.parse(String(response.body))["places"]
+    data::Dict{String,Any} = JSON.parse(String(response.body))
 
-    append!(places, data)
+    if length(data) > 0
+        return data["places"]
+    else
+        return []
 
+    end
+end
+
+# modfiy place dictionary into single level
+function _flattenplace(rawplace::Dict{String,Any})::Dict{String,Any}
+    if length(rawplace) > 0
+        place::Dict{String,Any} = Dict{String,Any}()
+        place["place_id"] = rawplace["id"]
+        place["lat"] = rawplace["location"]["latitude"]
+        place["lon"] = rawplace["location"]["longitude"]
+        typesdict::Dict{String,Bool} = _extracttypes(place_types=rawplace["types"])
+        place = merge(place, typesdict)
+
+        return place
+    else
+        return rawplace
+    end
+
+end
+
+# separate place types into boolean indicators
+function _extracttypes(; place_types::Vector{Any})::Dict{String,Bool}
+
+    output::Dict{String,Any} = Dict{String,Any}()
+    for type in all_types
+        if type in place_types
+            output[type] = true
+        else
+            output[type] = false
+        end
+
+    end
+
+    return output
 end
 
 end
--- get_place_data.jl
+++ get_place_data.jl
@@ -1,7 +1,22 @@
 include("PlaceDetailsSearch.jl")
 using .PlaceDetailsSearch
-using JSON
+import GeoIO
+import HTTP as HTTP
+
+google_api::GoogleAPI = GoogleAPI(
+    base="https://places.googleapis.com/v1/",
+    key="AIzaSyB7lJ3rfPvqYp8xbHGUEkoSEBx86Qy1lls"
+)
+
+response::HTTP.Response = HTTP.get("https://data.cityofnewyork.us/resource/7t3b-ywvw.geojson",
+    query=[
+        "\$\$app_token" => "PoXtpm9UpT6UnvUOK8F0lp3Sp",
+        # "\$query" => query_statement
+    ]
+)
+
+test = GeoIO.load(String(response.body))
 
 
-PlaceDetailsSearch.get_place_details()
-JSON.json(PlaceDetailsSearch.places)
+placedetails(google_api, test[2, :geometry], 100)
+
