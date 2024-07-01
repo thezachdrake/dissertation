23cf2880f77abb40efbe7850bb2aa70b7d15b188
Zach Drake <45634447+thezachdrake@users.noreply.github.com>
2024-07-01 16:01:52 -0400

    switching to GeoTables framework

--- cleaning_tools.jl
+++ cleaning_tools.jl
@@ -1,51 +1,55 @@
-crimecategories = ["VIOLENCE", "LARCENY", "DRUGS", "BURGLARY"]
+const crimecategories = ["VIOLENCE", "LARCENY", "DRUGS", "BURGLARY"]
 
-function fix_law_cd(law_cat_cd)::String
-    if typeof(law_cat_cd) == Missing
-        return ""
-    else
-        mappings = Dict{String,String}(
-            "F" => "FELONY",
-            "M" => "MISDEMEANOR",
-            "V" => "VIOLATION",
-            "I" => "VIOLATION",
-            "9" => "",
-        )
-        return get(mappings, String(law_cat_cd), String(law_cat_cd))
+map_law_cat(law_cat_cd::String)::String = @match law_cat_cd begin
+    "F" => "FELONY"
+    "M" => "MISDEMEANOR"
+    "V" => "VIOLATION"
+    "I" => "VIOLATION"
+    _   => ""
+end
+
+function filter_crime_types(row::TableTransforms.CTableRow)::Bool
+    return row."crime_cat" in crimecategories
+end
+
+function map_crime_cat(ofns_desc::String, law_cat::String)::String
+    return @match ofns_desc begin
+        ofns_desc where {occursin("LARCENY", ofns_desc) && !occursin("VEHICLE", ofns_desc)} => "LARCENY"
+        "MURDER & NON-NEGL. MANSLAUGHTE" => "VIOLENCE"
+        ofns_desc where {occursin("MURDER & NON-NEGL. MANSLAUGHTER", ofns_desc)} => "VIOLENCE"
+        ofns_desc where {occursin("ASSAULT", ofns_desc)} => "VIOLENCE"
+        ofns_desc where {occursin("ROBBERY", ofns_desc)} => "VIOLENCE"
+        ofns_desc where {occursin("BURGLARY", ofns_desc)} => "BURGLARY"
+        ofns_desc where {occursin("DRUGS", ofns_desc)} => "DRUGS"
+        ofns_desc where {!(ofns_desc in crimecategories) & (law_cat == "MISDEMEANOR")} => "ALL OTHER MISDEMEANORS"
+        ofns_desc where {!(ofns_desc in crimecategories) & (law_cat == "FELONY")} => "ALL OTHER FELONIES"
+        ofns_desc where {!(ofns_desc in crimecategories) & (law_cat == "VIOLATION")} => "ALL OTHER VIOLATIONS"
+        _ => string(ofns_desc)
     end
 end
 
-function filter_types(type::String)::Bool
-    return type in crimecategories
-end
+function filter_streets_manhattan(
+    borocode::String, rw_type::String, physicalid::String
+)::Bool
+    keep::Bool = true
+    @debug physicalid
 
-function map_offense_desc(ofns_desc, law_cat_cd)::String
-    if typeof(ofns_desc) == Missing
-        return ""
-    elseif ofns_desc == "MURDER & NON-NEGL. MANSLAUGHTE"
-        return "VIOLNCE"
-    elseif occursin("LARCENY", ofns_desc) & !occursin("VEHICLE", ofns_desc)
-        return "LARCENY"
-    elseif occursin("LARCENY", ofns_desc)
-        return "LARCENY"
-    elseif occursin("MURDER & NON-NEGL. MANSLAUGHTER", ofns_desc)
-        return "VIOLENCE"
-    elseif occursin("BURGLARY", ofns_desc)
-        return "BURGLARY"
-    elseif occursin("ASSAULT", ofns_desc)
-        return "VIOLENCE"
-    elseif occursin("ROBBERY", ofns_desc)
-        return "VIOLENCE"
-    elseif occursin("DRUGS", ofns_desc)
-        return "DRUGS"
-    elseif !(ofns_desc in crimecategories) & (law_cat_cd == "MISDEMEANOR")
-        return "ALL OTHER MISDEMEANORS"
-    elseif !(ofns_desc in crimecategories) & (law_cat_cd == "FELONY")
-        return "ALL OTHER FELONIES"
-    elseif !(ofns_desc in crimecategories) & (law_cat_cd == "VIOLATION")
-        return "ALL OTHER VIOLATIONS"
-    else
-        return string(ofns_desc)
+    if borocode != "1"
+        @debug "Is Manhattan"
+        keep = false
+        return keep
     end
 
+    if rw_type != "1"
+        @debug "Is Street"
+        keep = false
+        return keep
+    end
+
+    if physicalid == "174150"
+        keep = false
+        return keep
+    end
+
+    return keep
 end
--- main.jl
+++ main.jl
@@ -1,58 +1,41 @@
 using GeoStats
-import GeoIO
 using JSON
-using DataFrames
-include("GoogleMapsPlaceSearch.jl")
-using .GoogleMapsPlaceSearch
+import GeoIO: load
+import Base.Threads
+using Match
+import CairoMakie as Mke
+# include("GoogleMapsPlaceSearch.jl")
+# using .GoogleMapsPlaceSearch
 include("cleaning_tools.jl")
 
-arrests = GeoIO.load("data/arrests.geojson") |> DataFrame
-describe(arrests)
+### load and clean datasets
+incdients =
+    load("data/incidents.geojson") |>
+    Map(:law_cat_cd => map_law_cat => "law_cat") |>
+    Map([:ofns_desc, :law_cat] => map_crime_cat => "crime_cat") |>
+    Filter(filter_crime_types)
 
-incidents = GeoIO.load("data/incidents.geojson") |> DataFrame
-describe(incidents)
-
-incidents.crime_cat = map_offense_desc.(incidents.ofns_desc, incidents.law_cat_cd)
-filter!(:crime_cat => filter_types, incidents)
-combine(
-    groupby(incidents, :crime_cat),
-    :crime_cat => length => :count
-)
-unique(incidents.geometry)
-
-
-arrests.law_cat_cd = fix_law_cd.(arrests.law_cat_cd)
-arrests.crime_cat = map_offense_desc.(arrests.ofns_desc, arrests.law_cat_cd)
-filter!(:crime_cat => filter_types, arrests)
-combine(
-    groupby(arrests, :crime_cat),
-    :crime_cat => length => :count
-)
+arrests =
+    load("data/arrests.geojson") |>
+    Map(:law_cat_cd => map_law_cat => "law_cat") |>
+    Map([:ofns_desc, :law_cat] => map_crime_cat => "crime_cat") |>
+    Filter(filter_crime_types)
 
 place_data = []
-for place in readdir("data/places/")
-    push!(place_data, JSON.parsefile("data/places/" * place))
+@sync for place in readdir("data/places/")
+    Threads.@spawn push!(place_data, JSON.parsefile("data/places/" * place))
 end
 
-places = georef(
-    DataFrame(GoogleMapsPlaceSearch.flattenplace.(place_data)), [:lon, :lat]
-) |> DataFrame
+places = georef(DataFrame(GoogleMapsPlaceSearch.flattenplace.(place_data)), [:lon, :lat])
 
-
-streets = GeoIO.load("data/streets.geojson") |> DataFrame
-select_manhattan(borocode::String) = borocode == "1"
-select_streets(rw_type::String) = rw_type == "1"
-filter!(:borocode => select_manhattan, streets)
-filter!(:rw_type => select_streets, streets)
-select_bad_place(physicalid) = physicalid == "174150"
-filter(:physicalid => select_bad_place, streets)
+streets = load("data/streets.geojson")
+filter!([:borocode, :rw_type, :physicalid] => filter_streets_manhattan, streets)
 
 street_to_point = streets |> Transfer(incidents.geometry)
-incidents_street = hcat(street_to_point, georef(select(incidents, Not(:geometry)), incidents.geometry)) |> DataFrame
+incidents_street =
+    hcat(street_to_point, georef(select(incidents, Not(:geometry)), incidents.geometry)) |>
+    DataFrame
 
-x = combine(
-    groupby(incidents_street, :physicalid),
-    :physicalid => length => :count
-)
+x = combine(groupby(incidents_street, :physicalid), :physicalid => length => :count)
 
 sort(x, :count)
